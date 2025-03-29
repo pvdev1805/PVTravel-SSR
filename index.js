379a8c595923fb5a75f25f6a95a36f4435b43683
Phu Vo <u1153426@umail.usq.edu.au>
2025-03-29 16:03:00 +1000

    💥 Integrate PUG as template engine

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const port = 3000
 
+// Set up views directory and view engine PUG
+app.set('views', path.join(__dirname, 'views')) // Set the views directory containing PUG files
+app.set('view engine', 'pug') // Set PUG as the view engine
+
 app.get('/', (req, res) => {
-  res.send('Homepage')
+  res.render('client/pages/home', {
+    pageTitle: 'Homepage'
+  })
 })
 
 app.get('/tours', (req, res) => {
-  res.send('Tours List')
+  res.render('client/pages/tour-list', {
+    pageTitle: 'Tour List'
+  })
 })
 
 app.listen(port, () => {
