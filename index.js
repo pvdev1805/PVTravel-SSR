f30dfc457e092fdc4b6ddac50c30bb7030bfd2fa
Phu Vo <u1153426@umail.usq.edu.au>
2025-03-31 21:37:51 +1000

    🔨 Set up Home Controller and Tour Controller

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ dotenv.config()
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE)
 
-const Tour = require('./models/tour.model')
+const homeController = require('./controllers/client/home.controller')
+const tourController = require('./controllers/client/tour.controller')
 
 const app = express()
 
@@ -20,21 +21,9 @@ app.set('view engine', 'pug') // Set PUG as the view engine
 // Set up static files directory
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('client/pages/home', {
-    pageTitle: 'Homepage'
-  })
-})
+app.get('/', homeController.home)
 
-app.get('/tours', async (req, res) => {
-  const tourList = await Tour.find({})
-  console.log(tourList)
-
-  res.render('client/pages/tour-list', {
-    pageTitle: 'Tour List',
-    tourList: tourList
-  })
-})
+app.get('/tours', tourController.list)
 
 app.listen(port, () => {
   console.log(`App server is running on port ${port}`)
