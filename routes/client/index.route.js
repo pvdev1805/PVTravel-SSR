625f9dd5a55a9c33c0a5b0496f5524d4de7d2ebc
Phu Vo <u1153426@umail.usq.edu.au>
2025-04-02 23:23:12 +1000

    💄 Integrate Cart page UI

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const homeRoutes = require('./home.route')
 const tourRoutes = require('./tour.route')
+const cartRoutes = require('./cart.route')
 
 router.use('/', homeRoutes)
 router.use('/tours', tourRoutes)
+router.use('/cart', cartRoutes)
 
 module.exports = router
