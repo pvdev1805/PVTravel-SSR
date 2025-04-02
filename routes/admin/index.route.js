c87794bcd954db22e524df4acd3ed9b4a7701fd8
Phu Vo <u1153426@umail.usq.edu.au>
2025-04-03 01:38:43 +1000

    💄 Integrate Admin UI: Dashboard page

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const accountRoutes = require('./account.route')
+const dashboardRoutes = require('./dashboard.route')
 
 router.use('/account', accountRoutes)
+
+router.use('/dashboard', dashboardRoutes)
 
 module.exports = router
