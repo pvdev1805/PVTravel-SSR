eb52db5318bfbde12961ae2717c716c7684217bb
Phu Vo <u1153426@umail.usq.edu.au>
2025-05-31 19:25:50 +1000

    🔨 Handle Display data on Section 2 (Home - Client)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,26 @@
-module.exports.home = (req, res) => {
+const moment = require('moment')
+const Tour = require('../../models/tour.model')
+
+module.exports.home = async (req, res) => {
+  // Section 2
+  const tourListSection2 = await Tour.find({
+    priceNewAdult: { $gt: 0 },
+    deleted: false,
+    status: 'active'
+  })
+    .sort({
+      position: 'desc'
+    })
+    .limit(6)
+
+  tourListSection2.forEach((tour) => {
+    tour.discount = parseInt(((tour.priceAdult - tour.priceNewAdult) / tour.priceAdult) * 100)
+    tour.departureDateFormat = moment(tour.departureDate).format('DD/MM/YYYY')
+  })
+  // End - Section 2
+
   res.render('client/pages/home', {
-    pageTitle: 'Homepage'
+    pageTitle: 'Homepage',
+    tourListSection2: tourListSection2
   })
 }
