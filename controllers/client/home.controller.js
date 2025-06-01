50af4be8e6c631467d8621b5b998809f80da7052
Phu Vo <u1153426@umail.usq.edu.au>
2025-06-01 22:28:10 +1000

    🔨 Handle Display Domestic Tour list in Section 4 (Home - Client)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,7 @@
 const moment = require('moment')
 const Tour = require('../../models/tour.model')
 const Promotion = require('../../models/promotion.model')
+const Category = require('../../models/category.model')
 
 module.exports.home = async (req, res) => {
   // Section 2
@@ -25,9 +26,45 @@ module.exports.home = async (req, res) => {
   }
   // End - Section 2
 
+  // Section 4 - Domestic Tours
+  const domesticTour = await Category.findOne({
+    name: 'Domestic Tours'
+  })
+
+  const domesticTourId = domesticTour.id
+
+  const listCategory = [domesticTourId]
+
+  const listSubCategory = await Category.find({
+    parent: domesticTourId,
+    deleted: false,
+    status: 'active'
+  })
+
+  listSubCategory.forEach((subCategory) => {
+    listCategory.push(subCategory.id)
+  })
+
+  const tourListSection4 = await Tour.find({
+    category: { $in: listCategory },
+    deleted: false,
+    status: 'active'
+  })
+    .sort({
+      position: 'desc'
+    })
+    .limit(8)
+
+  for (const tour of tourListSection4) {
+    tour.discount = parseInt(((tour.priceAdult - tour.priceNewAdult) / tour.priceAdult) * 100)
+    tour.departureDateFormat = moment(tour.departureDate).format('DD/MM/YYYY')
+  }
+  // End - Section 4 - Domestic Tours
+
   res.render('client/pages/home', {
     pageTitle: 'Homepage',
     tourListSection2: tourListSection2,
+    tourListSection4: tourListSection4,
     promotionDetail: promotionDetail
   })
 }
